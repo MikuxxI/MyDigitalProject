a7aec6bb71f5006605396cb8b26275e14fd5609c
BorisM <boris.montron@gmail.com>
2022-07-06 18:36:36 +0200

    [feat] Ouverture modal discovery

--- FRONT/src/pages/home/discovery/discovery.ts
+++ FRONT/src/pages/home/discovery/discovery.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
-import { ViewController } from "ionic-angular";
+import { ModalController, ViewController } from "ionic-angular";
 import { Place } from "../../../models/Place";
+import { SingleBookPage } from "../single-book/single-book";
 
 @Component({
   selector: "discovery",
@@ -11,7 +12,8 @@ export class Discovery {
   public place: Place;
   public bg: String;
 
-  constructor(private viewCtrl: ViewController) {
+  constructor(private viewCtrl: ViewController,
+    private modalCtrl: ModalController) {
     if (this.place) {
       this.bg = this.place.photo;
     }
@@ -26,5 +28,13 @@ export class Discovery {
 
   dismissModal() {
     this.viewCtrl.dismiss();
+  }
+
+
+  onLoadLivre() {
+    let modal = this.modalCtrl.create(SingleBookPage, {
+      index: 0,
+    });
+    modal.present();
   }
 }
